fc9f3284d96a87b801813fd5cda9c34b3aa82223
Mike Ciechan <lndcobra@hotmail.com>
2024-10-20 22:52:59 +0100

    Removed Dependencies in App Service to slowly add back

--- DependencyInjectionGoneWrong/AppServices.cs
+++ DependencyInjectionGoneWrong/AppServices.cs
@@ -2,9 +2,5 @@
 
 public class AppServices
 {
-    public static ActivitySource Tracing { get; set; } = new ("DependencyInjectionGoneWrong");
-    public static CityNameProvider CityNameProvider { get; set; } = new ();
-    public static ConfigOptions Config  { get; set; } = new ();
-    public static HttpClient HttpClient { get; set; } = new();
-    public static TimeProvider Time { get; set; } = TimeProvider.System;
+    
 }
