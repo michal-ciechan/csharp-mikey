98130049c72f32c08299754b06a2606ba1c02c38
Mike Ciechan <lndcobra@hotmail.com>
2024-12-08 06:02:01 +0000

    Merge branch 'master' into branch
    # Conflicts:
    #	DependencyInjectionGoneWrong/Services/WeatherForecastService.cs

--- DependencyInjectionGoneWrong/Services/WeatherForecastService.cs
+++ DependencyInjectionGoneWrong/Services/WeatherForecastService.cs
@@ -9,8 +9,7 @@
 /// </summary>
 public class WeatherForecastService
 {
-    private static readonly Log Logger = LogManager.GetLogger<WeatherForecastService>();
-    
+    private static readonly Log Logger = LogManager.GetLogger(typeof(WeatherForecastService));
     private readonly IWeatherForecastCalculator _weatherForecastCalculator;
 
     public WeatherForecastService(
@@ -22,34 +21,33 @@ public class WeatherForecastService
     
     public async Task<WeatherForecast[]> GenerateWeatherForecast(int cityId)
     {
-        throw new NotImplementedException();
-        // AppServices.Tracing.StartActivity("GenerateWeatherForecast");
-        //
-        // Logger.Info("Generating weather forecast");
-        //
-        // var stopwatch = Stopwatch.StartNew();
-        // var cityName = AppServices.CityNameProvider.GetCityName(cityId);
-        //
-        // // Fetch Calculated and Remote Weather Forecasts
-        // var calculatedForecasts = await Task.Run(
-        //     () => _weatherForecastCalculator.GetWeather(cityId)
-        // );
-        //
-        // var remoteForecasts = await AppServices.HttpClient.GetFromJsonAsync<WeatherForecast[]>(
-        //     $"{AppServices.Config.RemoteServiceUrl}/weather?cityId={cityId}"
-        // );
-        //
-        // // Combine
-        // var result = calculatedForecasts.UnionBy(remoteForecasts!, x => x.Date).ToArray();
-        //
-        // // Save
-        // var filePath = Path.Combine(AppServices.Config.OutputPath, $"{cityId}.json");
-        // await File.WriteAllTextAsync(filePath, JsonSerializer.Serialize(result));
-        //
-        // Logger.Info(
-        //     $"Weather forecast for {cityName}[{cityId}] generated in {stopwatch.ElapsedMilliseconds}ms at {AppServices.TimeProvider.GetUtcNow()}"
-        // );
-        //
-        // return result;
+        AppServices.Tracing.StartActivity();
+        
+        Logger.Info("Generating weather forecast");
+        
+        var stopwatch = Stopwatch.StartNew();
+
+        // Fetch Calculated and Remote Weather Forecasts
+        var calculatedForecasts = await Task.Run(() => _weatherForecastCalculator.GetWeather(cityId));
+        
+        var remoteForecasts = await AppServices.HttpClient.GetFromJsonAsync<WeatherForecast[]>(
+            $"{AppServices.Config.RemoteServiceUrl}/weather?cityId={cityId}"
+        );
+        
+        // Combine
+        var result = calculatedForecasts.UnionBy(remoteForecasts!, x => x.Date).ToArray();
+        
+        // Save
+        var filePath = Path.Combine(AppServices.Config.OutputPath, $"{cityId}.json");
+        await File.WriteAllTextAsync(filePath, JsonSerializer.Serialize(result));
+
+        var elapsedMs = stopwatch.ElapsedMilliseconds;
+        var cityName = AppServices.CityNameProvider.GetCityName(cityId);
+        
+        Logger.Info(
+            $"Weather forecast for {cityName}[{cityId}] generated in {elapsedMs}ms at {AppServices.Time.GetUtcNow()}"
+        );
+        
+        return result;
     }
 }
